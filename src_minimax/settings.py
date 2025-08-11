968c31ea026c4c5fb3cd154ce5eeb6391517a3c1
Eliot Kasha <66979105+EliotKasha@users.noreply.github.com>
2025-08-10 23:26:44 -0700

    Update settings.py

--- src_minimax/settings.py
+++ src_minimax/settings.py
@@ -1,5 +1,5 @@
 # Lichess
-API_KEY = "lip_TUhLLItapV6OvuR4k1AW"  # Unique API key
+API_KEY = ""  # Unique API key
 USER = "TfXD"  # Account name that the model will train on
 ACC_NAME = "TfXD_Bot"  # Bot account name
 WELCOME_MSG = "Glhf! <3"  # Message sent on game start
@@ -10,4 +10,5 @@ AUTO_CHALLENGE = False
 CHALLENGE_USER = "TfXD"
 
 # Misc
+
 USE_BOOK = False
